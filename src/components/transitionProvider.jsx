d6b93da83640da142ede8772e4d4b73951ae6dfb
Hussam Hassan <hossam.hassan53@yahoo.com>
2024-09-26 21:33:23 +0300

    animation of pagess

--- src/components/transitionProvider.jsx
+++ src/components/transitionProvider.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, delay, motion } from "framer-motion";
 import { usePathname } from "next/navigation";
 import Navbar from "./navbar";
 
@@ -18,11 +18,19 @@ const TransitionPrividor = ({ children }) => {
           transition={{ duration: 0.5, ease: "easeOut" }}
         />
         <motion.div
-          className="h-screen w-screen fixed bg-black rounded-b-[100px] z-40"
+          className="fixed m-auto top-0 bottom-0 left-0 right-0 text-white text-8xl cursor-default  z-50 w-fit h-fit "
+          initial={{ opacity: 1 }}
+          animate={{ opacity: 0 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.8, ease: "easeOut" }}
+        >
+        {pathname.substring(1)}</motion.div>
+        <motion.div
+          className="h-screen w-screen fixed bg-black-500 rounded-t-[100px] bottom-0 z-40"
           initial={{ height: "140vh" }}
-          animate={{ height: "0vh",transtion: }}
+          animate={{ height: "0vh",transtion:{delay:0.8} }}/>
           
-        <div className="w-screen h-screen bg-gradient-to-b from-blue-100 to-red-100 ">
+        <div className="w-screen h-screen bg-gradient-to-b from-blue-100 to-red-100">
           <div className="h-24">
             <Navbar />
           </div>
